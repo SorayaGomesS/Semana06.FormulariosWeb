0ce1da766f1eb6daa1325994e19a14f3680338d5
SorayaGomesS <sorayagomes62@gmail.com>
2025-09-18 13:59:31 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,42 +1,42 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request
 from datetime import datetime
+import socket
 
 app = Flask(__name__)
 
-dados_usuario = {
-    "nome": None,
-    "sobrenome": None,
+user_data = {
+    "nome": "Estranho",
     "instituicao": None,
-    "disciplina": None
+    "disciplina": None,
+    "ip": None,
+    "host": None
 }
+
 
 @app.route("/", methods=["GET", "POST"])
 def home():
-    global dados_usuario
-
     if request.method == "POST":
-        dados_usuario["nome"] = request.form.get("nome")
-        dados_usuario["sobrenome"] = request.form.get("sobrenome")
-        dados_usuario["instituicao"] = request.form.get("instituicao")
-        dados_usuario["disciplina"] = request.form.get("disciplina")
+        user_data["nome"] = request.form.get("nome", "Estranho")
+        user_data["instituicao"] = request.form.get("instituicao", None)
+        user_data["disciplina"] = request.form.get("disciplina", None)
+        user_data["ip"] = request.remote_addr
+        user_data["host"] = socket.gethostname()
 
-    return render_template("index.html", dados=dados_usuario)
+    return render_template("home.html", user=user_data)
 
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    username = None
+    local_time = None
+
     if request.method == "POST":
-        usuario = request.form.get("usuario")
-        return redirect(url_for("acesso", usuario=usuario))
-    return render_template("login.html")
+        username = request.form.get("username")
+        local_time = datetime.now().strftime("%B %d, %Y %I:%M %p")
 
-
-@app.route("/acesso")
-def acesso():
-    usuario = request.args.get("usuario", "desconhecido")
-    now = datetime.now().strftime("%B %d, %Y %I:%M %p")
-    return render_template("acesso.html", usuario=usuario, now=now)
+    return render_template("login.html", username=username, local_time=local_time)
 
 
 if __name__ == "__main__":
     app.run(debug=True)
+
